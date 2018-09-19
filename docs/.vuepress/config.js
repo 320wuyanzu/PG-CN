5ce804294b5e55f2358f300eecc0fb963ebf91f6
320wuyanzu <wuyanzu320@outlook.com>
2018-09-19 22:27:57 +0800

    改用折叠式目录

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -15,12 +15,52 @@ module.exports = {
             { text: '应用层', link:'/6/'},
             { text: '网络安全', link:'/7/'},
             { text: '视频、音频与无线网络', link:'/8/'},
-            { text: '下一代因特网', link:'/9/'},
-
+            { text: '下一代因特网', link:'/9/'}
         ],
-        sidebarDepth:2,
-        sidebar: sidebar_divide()
+        sidebarDepth: 2,
+        sidebar: sidebar_group()
     }
+}
+
+
+function sidebar_group(){
+    let C1 = {
+        title:'第一章 - 概述',
+        children:['/1/']
+    };
+    let C2 = {
+        title:'第二章 - 物理层',
+        children:['/2/']
+    };
+    let C3 = {
+        title:'第三章 - 数据链路层',
+        children:['/3/']
+    };
+    let C4 = {
+        title:'第四章 - 网络层',
+        children:['/4/']
+    };
+    let C5 = {
+        title:'第五章 - 传输层',
+        children:['/5/']
+    };
+    let C6 = {
+        title:'第六章 - 应用层',
+        children:['/6/']
+    };
+    let C7 = {
+        title:'第七章 - 网络安全',
+        children:['/7/']
+    };
+    let C8 = {
+        title:'第八/九章 - 视频、音频与无线网络',
+        children:['/8/']
+    };
+    let C9 = {
+        title:'第十章 - 下一代因特网',
+        children:['/9/']
+    };
+    return [C1,C2,C3,C4,C5,C6,C7,C8,C9];
 }
 function sidebar_divide(){
     return{
--- docs/8/README.md
+++ docs/8/README.md
@@ -1,16 +1,24 @@
----
-sidebar: auto
-prev: /7/
-next: /9/
----
-# 第八章 -视频、音频与无线网络
+
+# 第八/九章 -视频、音频与无线网络
 
 ## 1、RTP、RTCP、QoS等的概念
 
+RTP-实时运输协议：  
+RTCP-实时运输控制协议：  
+QoS-服务质量：  
+
 ## 2、时延、时延抖动等的处理
 
-## 3、WLan、WPan、WMan、WLL的概念
+## 3、WLAN、WPAN、WMAN、WLL的概念
+
+WLAN-无线局域网  
+WPAN-无线个人区域网  
+WMAN-无线城域网  
+
 
 ## 4、无线局域网的DCF和PCF
+
+DCF-分布协调功能  
+PCF-点协调功能  
 
 ## 5、CSMA/CA协议的原理
